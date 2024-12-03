e720488822a00a30cba5e68e6e40a2a1c41b7ea9
FrankOneMillion <145765838+FrankOneMillion@users.noreply.github.com>
2024-12-03 15:39:09 +0000

    added additional efficiency code
    improvments but still not final form

--- Assets/scripts/playerMovement.cs
+++ Assets/scripts/playerMovement.cs
@@ -3,42 +3,40 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public float speed;
-    private float acceleration;
 
     public Rigidbody rb;
+    public float move;
 
 
     void Start()
     {
+        move = 0;
         speed = 35.0f;
-        acceleration = 0.0f;
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (acceleration < 0.0f) { acceleration += 0.05f; };
-        //if (acceleration > 0.0f) { acceleration *= 0.99f; acceleration -= 0.05f; };
-        acceleration = 0.0f;
+        rb.AddForce(transform.forward * move * speed);
     }
     void FixedUpdate()
     {
-
+        if (Input.GetKeyUp(KeyCode.W)) {
+            move = 0.0f;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            move = 0.0f;
+        }
         //"wasd" movement controls
         if (Input.GetKey(KeyCode.W))
         {
-            acceleration += 0.06f;
-            acceleration = acceleration / 0.99f;
-            if (acceleration < 1.0f) { acceleration *= 1.9f; };
-            rb.AddForce(transform.forward * speed * acceleration);
+            move = 1.0f;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            acceleration += 0.06f;
-            acceleration = acceleration / 0.99f;
-            if (acceleration < 1.0f) { acceleration *= 1.2f; };
-            rb.AddForce(-transform.forward * speed * acceleration);
+            move = -1.0f;
         }
         if (Input.GetKey(KeyCode.A))
         {

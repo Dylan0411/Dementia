b4e48c32180ceb7f943405d8165068e735b93993
Dylan0411 <dpwhitney535@gmail.com>
2025-02-06 21:59:20 +0000

    (1) added parts of puzzle concept 2
    - made a teapot model instead of a vase and broke it into pieces as well as adding a 'ghost' teapot to make it easier for the player to know what to do. (non functional but proves a point)

--- Assets/scripts/puzzle 2/brokenItem.cs
+++ Assets/scripts/puzzle 2/brokenItem.cs
@@ -9,28 +9,34 @@ public class brokenItem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A)|| Input.GetKeyDown(KeyCode.S)|| Input.GetKeyDown(KeyCode.D)) //exit table
+        if (tableInterface.usingTable == true)
         {
-            //lock and hide the cursor
-            Cursor.lockState = CursorLockMode.Locked;//let the player move the cursor
-            Cursor.visible = false; //hide cursor
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D)) //exit table
+            {
+                //lock and hide the cursor
+                Cursor.lockState = CursorLockMode.Locked;//let the player move the cursor
+                Cursor.visible = false; //hide cursor
 
-            //switch cameras
-            tableCamera.SetActive(false);
-            mainPlayerCamera.SetActive(true);
+                //switch cameras
+                tableCamera.SetActive(false);
+                mainPlayerCamera.SetActive(true);
 
-            //bring back the players body
-            player.SetActive(true);
+                //bring back the players body
+                player.SetActive(true);
 
-            tableInterface.usingTable = false;  //mark the table as not being used (allows the player to walk around etc)
+                tableInterface.usingTable = false;  //mark the table as not being used (allows the player to walk around etc)
+            }
+
+            //interacting with broken vase code here
+
+
+
+
+
+
+
+
+
         }
-
-
-        //interacting with broken vase code here
-
-
-
-
-
     }
 }

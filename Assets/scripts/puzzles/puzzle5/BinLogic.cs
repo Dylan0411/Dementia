4772ba7282454a4af436d88efe94141ae2a836e8
Dylan0411 <dpwhitney535@gmail.com>
2025-05-06 21:35:51 +0100

    added particle effects for puzzle 5

--- Assets/scripts/puzzles/puzzle5/BinLogic.cs
+++ Assets/scripts/puzzles/puzzle5/BinLogic.cs
@@ -1,47 +1,40 @@
-using NUnit.Framework;
-using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class BinLogic : MonoBehaviour
 {
+    public GameObject explosionParticles; // Reference to the explosion prefab
     [SerializeField] List<int> validIDs;
     public bool puzzleCompleteFlag = false;
     public bool resetPuzzleFlag = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("canPickup"))
+        if (other.gameObject.CompareTag("canPickup"))
         {
             puzzleConcept1_Item item = other.gameObject.GetComponent<puzzleConcept1_Item>();
-            if(item != null )
+            if (item != null)
             {
-                if(validIDs.Contains(item.idNumber))
+                if (validIDs.Contains(item.idNumber))
                 {
                     bool flag = validIDs.Remove(item.idNumber);
-                    if(flag)
-                    {
-                        Debug.Log("Removed item successfully");
-                    }
-                    else
-                    {
-                        Debug.Log("Item unable to be removed or item not found in list");
-                    }
+                    Debug.Log(flag ? "Removed item successfully" : "Item unable to be removed or item not found in list");
+
+                    // Instantiate explosion effect before destroying the object
+                    Instantiate(explosionParticles, other.transform.position, other.transform.rotation);
 
                     Destroy(other.gameObject);
 
-                    if(validIDs.Count == 0)
+                    if (validIDs.Count == 0)
                     {
                         puzzleCompleteFlag = true;
                         Debug.Log("Puzzle Complete");
-                        PlayerPrefs.SetInt("puzzle5Status", 1);//mark as complete
+                        PlayerPrefs.SetInt("puzzle5Status", 1); // Mark as complete
                     }
-
                 }
                 else
                 {
                     resetPuzzleFlag = true;
-                    //implement logic for resetting puzzle? needs discussion on if we do even do this, im not sure
                 }
             }
             else
